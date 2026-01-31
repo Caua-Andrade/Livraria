4dc6067e7094d9a2469679807018465bebab23d4
Caua-Andrade <cauaandsilva@gmail.com>
2026-01-30 21:38:18 -0300

    importei tudo que eu vou usar do lombok

--- src/main/java/com/caua/livraria/entities/Livro.java
+++ src/main/java/com/caua/livraria/entities/Livro.java
@@ -1,4 +1,19 @@
 package com.caua.livraria.entities;
 
-public class Livro {
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+public class Livro implements Serializable {
+
+    private Long id;
+    private String nome;
+    //private Autor autor;
 }
